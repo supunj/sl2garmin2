0ad227affadff4f95c2170e2a94a580cc6f5dede
Supun Jayathilake <supunj@gmail.com>
2021-01-25 17:11:53 +0530

    Re-factored code

--- build_map.sh
+++ build_map.sh
@@ -21,6 +21,7 @@ function merge_all()
 	echo $MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS -jar $MKGMAP --code-page=1252 --keep-going --gmapsupp --tdbfile --index $IMG_STRING $TYP_FILE
 	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS -jar $MKGMAP --code-page=1252 --keep-going --gmapsupp --tdbfile --index $IMG_STRING $TYP_FILE
 	echo 'Combining done.'
+	cd $MAP_ROOT
 }
 
 function prepare()
@@ -47,6 +48,7 @@ function prepare()
 	COASTLINE=$MAP_ROOT/maps/sri_lanka_coastline_v6.osm
 	STYLE_TRANSPORT=sl
 	STYLE_CONTOURS=sl
+	TYP_FILE=$TYP_LOC/os50_mod.typ
 
 	export MKGMAP_JAVACMD=/usr/bin/java
 	export MKGMAP_JAVACMD_OPTIONS="-Xmx4096M -jar -enableassertions"
@@ -65,40 +67,10 @@ function prepare()
 	else 
     		echo "Downloading latest map...."
 	fi
-
-	echo "Extracting ways and polygons..."
-        $OSMOSIS --read-pbf-fast file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf accept-ways --tf reject-ways natural=coastline --tf accept-relations --used-node --write-pbf $TEMP_LOC/sri-lanka-latest-transport.osm.pbf
-	echo "Extracting ways and polygons...done."
-
-	echo "Extracting POIs..."
-        $OSMOSIS --read-pbf-fast file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf accept-nodes amenity=* tourism=* highway=* leisure=* historic=* natural=* railway=* shop=* public_transport=* barrier=* man_made=* landmark=* natural=* sport=* --tf reject-ways --tf reject-relations --write-pbf $TEMP_LOC/sri-lanka-latest-poi.osm.pbf
-	echo "Extracting POIs...done."
-	
-	echo 'Splitting....'
-	cd $SPLIT_LOC
-	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $SPLITTER $TEMP_LOC/sri-lanka-latest.osm.pbf
-	echo 'Splitting done.'
-	cd $MAP_ROOT
-
-	#cp $MAP_ROOT/maps/srtm/* $SPLIT_LOC
-
-	#cat $TYP_LOC/os50mod/header.txt $TYP_LOC/os50mod/polygon.txt $TYP_LOC/os50mod/line.txt $TYP_LOC/os50mod/poi.txt > $TEMP_LOC/typ/os50mod.txt
-	#TYP_FILE=$TEMP_LOC/typ/os50mod.txt
-	#cat $TYP_LOC/os50mod/header.txt $TYP_LOC/os50mod/polygon.txt > $TEMP_LOC/typ/os50mod.txt
-	#TYP_FILE=$TEMP_LOC/typ/jbm.txt
-	TYP_FILE=$TYP_LOC/os50_mod.typ
 }
 
 function create_gpi()
 {
-        #for tag in "amenity" "tourism" "highway" "leisure" "historic" "natural" "railway" "shop" "public_transport" "barrier" "man_made" "landmark" "natural" "sport"
-        #do
-        #        echo "Extracting POIs - $tag"
-        #        $OSMOSIS --read-pbf file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf reject-ways --tf reject-relations --tf accept-nodes $tag=* --write-xml $TEMP_LOC/poi/sri-lanka-latest.osm_$tag.osm
-	#	gpsbabel -i osm -f $TEMP_LOC/poi/sri-lanka-latest.osm_$tag.osm -o garmin_gpi,category=$tag,bitmap=$ICON_LOC/$tag.bmp -F $GPI_LOC/$tag.gpi
-        #done	
-	#echo "Done!"
-
 	$OSMOSIS --read-pbf file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf reject-ways --tf reject-relations --node-key-value keyValueList="amenity.school" --write-xml file=$TEMP_LOC/school.osm
 	gpsbabel -i osm -f $TEMP_LOC/school.osm -o garmin_gpi,category="Schools",alerts=1,proximity=1km -F $GPI_LOC/school.gpi
 
@@ -109,28 +81,73 @@ function create_gpi()
 	gpsbabel -i osm -f $TEMP_LOC/signals.osm -o garmin_gpi,category="Traffic Lights",alerts=1,proximity=0.5km -F $GPI_LOC/signals.gpi
 }
 
+function build_sea_and_the_land()
+{
+	echo "Extracting the coastline..."
+        $OSMOSIS --read-pbf-fast file=$TEMP_LOC/sri-lanka-latest.osm.pbf --way-key-value keyValueList="natural.coastline" --tf reject-relations --used-node --write-pbf $TEMP_LOC/sri-lanka-latest-coastline.osm.pbf
+	echo "Extracting the coastline...done."
+	
+	cd $IMG_LOC
+	echo 'Sea and the land....'
+	IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
+	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/sea_land.args --dem=$HGT_LOC --description=Contours --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_CONTOURS $TEMP_LOC/sri-lanka-latest-coastline.osm.pbf
+	echo 'Sea and the land........done'
+	cd $MAP_ROOT
+}
+
+function build_contour_lines()
+{
+	cd $IMG_LOC
+	echo 'Generating contour lines....'
+	IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
+	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/elevation.args --description=Contours --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_CONTOURS $MAP_ROOT/maps/srtm/*.osm.pbf
+	echo 'Generating contour lines....done'
+	cd $MAP_ROOT
+}
+
+function build_roads_and_areas()
+{
+	echo "Extracting ways and polygons..."
+        $OSMOSIS --read-pbf-fast file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf accept-ways --tf reject-ways natural=coastline --tf accept-relations --used-node --write-pbf $TEMP_LOC/sri-lanka-latest-transport.osm.pbf
+	echo "Extracting ways and polygons...done."
+	
+	echo 'Splitting....'
+	cd $SPLIT_LOC
+	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $SPLITTER $TEMP_LOC/sri-lanka-latest-transport.osm.pbf
+	echo 'Splitting done.'
+	cd $MAP_ROOT
+	
+	cd $IMG_LOC
+	echo 'Generating transport map....'
+	IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
+	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/transport_osm.args --description=Transport --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT $SPLIT_LOC/*.osm.pbf
+	#$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/transport_osm.args --description=Transport --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT $SPLIT_LOC/*.osm.pbf
+	#$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/combined.args --description=Transport --dem=$HGT_LOC --coastlinefile=$COASTLINE --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT $SPLIT_LOC/*.osm.pbf
+	echo 'Generating transport map....done'
+	cd $MAP_ROOT
+}
+
+function build_pois()
+{	
+	echo "Extracting POIs..."
+        $OSMOSIS --read-pbf-fast file=$TEMP_LOC/sri-lanka-latest.osm.pbf --tf accept-nodes amenity=* tourism=* highway=* leisure=* historic=* natural=* railway=* shop=* public_transport=* barrier=* man_made=* landmark=* natural=* sport=* --tf reject-ways --tf reject-relations --write-pbf $TEMP_LOC/sri-lanka-latest-poi.osm.pbf
+	echo "Extracting POIs...done."
+	
+	cd $IMG_LOC
+	echo 'Generating POI map....'
+	IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
+	$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/poi.args --description=POI --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT --input-file=$TEMP_LOC/sri-lanka-latest-poi.osm.pbf
+	echo 'Generating POI map....done'
+	cd $MAP_ROOT
+}
+
 # ------------- Start -------------
 prepare
+build_sea_and_the_land
+build_contour_lines
+build_roads_and_areas
+build_pois
 #create_gpi
-
-# Start building maps
-cd $IMG_LOC
-
-echo 'Generating transport map....'
-IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
-$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/transport_osm.args --description=Transport --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT $SPLIT_LOC/*.osm.pbf
-echo 'Generating transport map....done'
-
-echo 'Generating POI map....'
-IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
-$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/poi.args --description=POI --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_TRANSPORT --input-file=$TEMP_LOC/sri-lanka-latest-poi.osm.pbf
-echo 'Generating POI map....done'
-
-echo 'Generating contour lines....'
-IMG_FILE_NAME="`shuf -i 10000000-99999999 -n 1`"
-$MKGMAP_JAVACMD $MKGMAP_JAVACMD_OPTIONS $MKGMAP -c $ARG_LOC/elevation.args --dem=$HGT_LOC --description=Contours --coastlinefile=$COASTLINE --mapname=$IMG_FILE_NAME --style-file=$STYLE_LOC --style=$STYLE_CONTOURS $MAP_ROOT/maps/srtm/*.osm.pbf
-echo 'Generating contour lines....done'
-
 merge_all
 #send_map
 # ------------- End -------------
